f2759a32570ff11618ff38431ff7b7b7aa926151
HumbleToS <ileakpics2020@gmail.com>
2022-12-27 03:58:11 -0700

    use a bot subclass for the use of setup_hook this is because the on_ready event shouldn't really be used for anything

--- main.py
+++ main.py
@@ -3,8 +3,10 @@
 # Want more bots? https://discord.gg/mWSRTKTvMq
 
 
-from asyncio import sleep
-from os import listdir
+import logging
+import logging.handlers
+import sys
+from pathlib import Path
 
 import discord
 from discord.ext import commands
@@ -12,31 +14,52 @@ from discord.ext import commands
 import settings
 from database import create_database
 
-bot = commands.Bot(
-    command_prefix=settings.prefix, description=settings.description, intents=discord.Intents.all()
-)  # Creates the bot, with all intents
-bot.remove_command('help')  # Removes the default help command for the custom one
+
+def setup_logging():
+    # Logging credit: Fretgfr
+    log_fmt = logging.Formatter(
+        fmt="%(asctime)s - %(name)s:%(lineno)d - %(levelname)s - %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S",
+    )
+
+    sh = logging.StreamHandler(sys.stdout)
+    sh.setLevel(logging.DEBUG)
+    sh.setFormatter(log_fmt)
+
+    root_logger = logging.getLogger()
+    root_logger.setLevel(logging.INFO)
+    root_logger.addHandler(sh)
 
 
-@bot.event
-async def on_ready():
-    create_database()  # Creates the database if it doesn't exist
+_logger = logging.getLogger(__name__)
 
-    loaded = 0
 
-    for file in listdir('./cogs'):  # Loops through all files in the cogs folder
-        if file.endswith('.py'):  # Check if its a python file
+class MyBot(commands.Bot):
+    def __init__(self) -> None:
+        super().__init__(
+            command_prefix=settings.prefix,
+            description=settings.description,
+            intents=discord.Intents.all(),
+            case_insensitive=True,  # This is so commands aren't case-sensitive
+        )
+
+    async def setup_hook(self) -> None:
+        create_database()  # This database setup is blocking and you should be using aiosqlite
+        self.remove_command('help')
+
+        for file in sorted(Path("cogs").glob("**/*.py")):
+            ext = ".".join(file.parts).removesuffix(".py")
             try:
-                await bot.load_extension(f'cogs.{file[:-3]}')  # Load the command
-            except Exception as e:
-                print(f'Failed to load file: {file} with error: {e}')
-            else:
-                print(f'Loaded File: {file}')  # Print that the command has been loaded
-                loaded += 1
-    print("Loaded a total of {0} files.".format(loaded))
-    await sleep(3)
-    print("Bot is ready!\nPrefix: {0}\nDescription: {1}".format(settings.prefix, settings.description))
+                await self.load_extension(ext)
+                _logger.info("Extension: %s loaded successfully", ext)
+            except Exception as error:
+                _logger.exception("Extension: %s failed to load\n%s", ext, error)
+
+        _logger.info("Loaded %s cogs!", len(self.cogs))
+        _logger.info("\nBot is ready!\nPrefix: %s\nDescription %s", settings.prefix, settings.description)
 
 
 if __name__ == "__main__":  # Checks if the file is being ran directly
+    setup_logging()
+    bot = MyBot()
     bot.run(settings.token)
